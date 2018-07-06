332fb51b9d808db65adca426a8f725b22d9e64fa
Bryce Wesie <bwesie2209@gmail.com>
2018-07-06 15:01:04 -0500

    This is a small app using MVC Architecture to keep a log of destinations and associated activities
    This is a small app using MVC Architecture to keep a log of destinations and associated activities

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :activities
+  resources :states
+  get 'ilovetocode' => 'states#index'
+  root 'states#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
